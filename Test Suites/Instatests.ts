5b9423c72cffd99bb1818fbc39b5b0a3e8cc2b3d
SOBHAREDDY PITTU <102750581+SR-Pittu@users.noreply.github.com>
2024-02-28 12:39:58 -0600

    adding new test suite

---
+++ Test Suites/Instatests.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Instatests</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>84334d9e-ac47-4e40-9872-0941fea7fce3</testSuiteGuid>
+   <testCaseLink>
+      <guid>7be66bea-bbc5-44f8-8644-d34af9118486</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCaseInstagram</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
